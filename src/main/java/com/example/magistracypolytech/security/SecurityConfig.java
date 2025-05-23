92b0bd6d4fded057096308dbb2431537c80262e0
moroooow <vovaspiridonov92@gmail.com>
2025-05-23 20:47:12 +0300

    fix

--- src/main/java/com/example/magistracypolytech/security/SecurityConfig.java
+++ src/main/java/com/example/magistracypolytech/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:3000","http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
